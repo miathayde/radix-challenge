08ab61fdc5879f28656163e9520e2e5674a4d121
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-27 20:05:18 -0300

    Criação de componente register-resume e sua rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { InitialPageComponent } from './initial-page/initial-page.component';
+import { RegisterResumeComponent } from './register-resume/register-resume.component';
 
 export const routes: Routes = [
-  {path: '', component: InitialPageComponent}
+  {path: '', component: InitialPageComponent},
+  {path: 'register-resume', component: RegisterResumeComponent}
 ];
---
+++ src/app/register-resume/register-resume.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-resume',
+  imports: [],
+  templateUrl: './register-resume.component.html',
+  styleUrl: './register-resume.component.scss'
+})
+export class RegisterResumeComponent {
+
+}
