82e496da43559740d6a45356fdcc349b69ec887e
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-27 20:14:21 -0300

    Inclusão de rotas nos botões do header e da página inicial

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
--- src/app/initial-page/initial-page.component.ts
+++ src/app/initial-page/initial-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-initial-page',
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './initial-page.component.html',
   styleUrl: './initial-page.component.scss'
 })
