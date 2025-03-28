69f5314a5179bd3d386ae6c82d0a34f75b9dd0fc
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-27 23:15:47 -0300

    Criação de model recume

---
+++ src/models/resume.ts
@@ -0,0 +1,14 @@
+export class ResumeModel {
+  name: string;
+  lastName: string;
+  birthday: Date;
+  gender: string;
+  phone: string;
+  email: string;
+  isBrasilian: boolean;
+  cpf: string;
+  hasDisability: boolean;
+  disability: string = '';
+  linkedIn: string;
+  github: string;
+}
