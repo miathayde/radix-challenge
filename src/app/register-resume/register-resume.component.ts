5ab642bd2c64878e9e9bff562e26b24dbc4f04ea
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-27 23:15:58 -0300

    Aplicando parte do formulário

--- src/app/register-resume/register-resume.component.ts
+++ src/app/register-resume/register-resume.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ResumeModel } from '../../models/resume';
+import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-register-resume',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule
+  ],
   templateUrl: './register-resume.component.html',
   styleUrl: './register-resume.component.scss'
 })
 export class RegisterResumeComponent {
+  resume: ResumeModel = new ResumeModel();
+  genderOptions: Array<string> = [
+    "Mulher cisgênero", "Mulher transgênero", "Homem cisgênero", "Homem transgênero", "Outro", "Prefiro não responder"
+  ];
 
 }
