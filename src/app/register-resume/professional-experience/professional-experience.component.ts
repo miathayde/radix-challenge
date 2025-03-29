b50d9e3978c397c0fb9be0d1265bc469dbfa157b
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-29 19:54:55 -0300

    Criação de componentes de professional-experience

---
+++ src/app/register-resume/professional-experience/professional-experience.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professional-experience',
+  imports: [],
+  templateUrl: './professional-experience.component.html',
+  styleUrl: './professional-experience.component.scss'
+})
+export class ProfessionalExperienceComponent {
+
+}
