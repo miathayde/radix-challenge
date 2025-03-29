f4816d141bcc1443345e925a52dd5be3c7b9090b
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-28 22:17:36 -0300

    Ajustando componente register-resume para componente com abas

--- src/app/register-resume/register-resume.component.ts
+++ src/app/register-resume/register-resume.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
+import { PersonalDataComponent } from "./personal-data/personal-data.component";
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { ResumeModel } from '../../models/resume';
-import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { PersonalDataModel } from '../../models/personal-data';
 
 @Component({
   selector: 'app-register-resume',
+  standalone: true,
   imports: [
     CommonModule,
+    MatTabsModule,
+    PersonalDataComponent,
     FormsModule,
     MatButtonModule,
     MatInputModule,
@@ -21,15 +27,17 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatRadioModule
   ],
   templateUrl: './register-resume.component.html',
   styleUrl: './register-resume.component.scss'
 })
 export class RegisterResumeComponent {
-  resume: ResumeModel = new ResumeModel();
-  genderOptions: Array<string> = [
-    "Mulher cisgênero", "Mulher transgênero", "Homem cisgênero", "Homem transgênero", "Outro", "Prefiro não responder"
-  ];
+  personalData: PersonalDataModel = new PersonalDataModel();
+  selectedTabIndex: number = 1;
 
+  setPersonalData(data: PersonalDataModel) {
+    this.personalData = data;
+  }
 }
