d8db91920fd6f383aeb159e22028389fa9252328
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-29 01:42:31 -0300

    Inclusão do MatIcon e criação de funções de 'nextTab' e 'cleanAll'

--- src/app/register-resume/register-resume.component.ts
+++ src/app/register-resume/register-resume.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { PersonalDataComponent } from "./personal-data/personal-data.component";
 import { MatButtonModule } from '@angular/material/button';
@@ -12,6 +12,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { PersonalDataModel } from '../../models/personal-data';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-register-resume',
@@ -28,16 +29,25 @@ import { PersonalDataModel } from '../../models/personal-data';
     MatDatepickerModule,
     MatNativeDateModule,
     MatCheckboxModule,
-    MatRadioModule
+    MatRadioModule,
+    MatIconModule
   ],
   templateUrl: './register-resume.component.html',
   styleUrl: './register-resume.component.scss'
 })
 export class RegisterResumeComponent {
   personalData: PersonalDataModel = new PersonalDataModel();
-  selectedTabIndex: number = 1;
+  selectedTabIndex: number = 0;
 
   setPersonalData(data: PersonalDataModel) {
     this.personalData = data;
+  }
+
+  nextTab() {
+    this.selectedTabIndex = 1;
+  }
+
+  cleanAll() {
+    this.personalData = new PersonalDataModel();
   }
 }
