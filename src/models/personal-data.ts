9fc5d21f02746d91d53997cc5d81693ba22d8de3
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-29 02:07:21 -0300

    Inicializando variaveis do objeto

--- src/models/personal-data.ts
+++ src/models/personal-data.ts
@@ -1,14 +1,14 @@
 export class PersonalDataModel {
-  name: string;
-  lastName: string;
-  birthday: Date;
-  gender: string;
-  phone: string;
-  email: string;
-  isBrasilian: boolean;
-  cpf: string;
-  hasDisability: boolean;
+  name: string = '';
+  lastName: string = '';
+  birthday: Date = new Date();
+  gender: string = '';
+  phone: string = '';
+  email: string = '';
+  isBrasilian: boolean = false;
+  cpf: string = '';
+  hasDisability: boolean | null = null;
   disability: string = '';
-  linkedIn: string;
-  github: string;
+  linkedIn: string = '';
+  github: string = '';
 }
