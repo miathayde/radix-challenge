cb4b4a590c2a3d07c8a92fa2f1445b19c0aaea7e
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-30 18:16:47 -0300

    Incluindo ngx mask no app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideEnvironmentNgxMask } from 'ngx-mask';
 import { routes } from './app.routes';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 
@@ -8,6 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'}
+    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'},
+    provideEnvironmentNgxMask()
   ]
 };
