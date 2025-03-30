b46f8f1c1a5ae8c234d47fdfb74821a2740b6aaa
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-29 22:36:50 -0300

    Ajustes na model de professional-experience

--- src/models/professional-experience.ts
+++ src/models/professional-experience.ts
@@ -1,9 +1,12 @@
 export class ProfessionalExperienceModel {
   company: string = '';
-  initialDate: Date = new Date();
-  endDate: Date = new Date();
+  initialMonth: string = '';
+  initialYear: number | null = null;
+  endMonth: string = '';
+  endYear: number | null = null;
   position: string = '';
   country: string = '';
   workModel: string = '';
+  actualWork: boolean = false;
   description: string = '';
 }
