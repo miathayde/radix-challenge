931cebb59cb07163ae9b8276a1c0f0fca3956136
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-30 18:15:47 -0300

    Colocando 'required' em campos obrigatórios e preenchendo link do github e linkedIn

--- src/app/register-resume/personal-data/personal-data.component.ts
+++ src/app/register-resume/personal-data/personal-data.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -10,6 +10,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { PersonalDataModel } from '../../../models/personal-data';
+import { NgxMaskDirective } from 'ngx-mask';
+
+const urlLinkedin: string = "https://linkedin.com/in/";
+const urlGithub: string = "https://github.com/";
 
 @Component({
   selector: 'app-personal-data',
@@ -24,19 +28,37 @@ import { PersonalDataModel } from '../../../models/personal-data';
     MatDatepickerModule,
     MatNativeDateModule,
     MatCheckboxModule,
-    MatRadioModule
+    MatRadioModule,
+    NgxMaskDirective
   ],
   templateUrl: './personal-data.component.html',
   styleUrl: './personal-data.component.scss'
 })
-export class PersonalDataComponent {
+export class PersonalDataComponent implements OnInit{
   @Output() data: EventEmitter<PersonalDataModel> = new EventEmitter<PersonalDataModel>();
   @Input() personalData: PersonalDataModel = new PersonalDataModel();
   genderOptions: Array<string> = [
     "Mulher cisgênero", "Mulher transgênero", "Homem cisgênero", "Homem transgênero", "Outro", "Prefiro não responder"
   ];
 
+  ngOnInit(): void {
+    this.setUrlInputs();
+  }
+
   onFormsChange() {
     this.data.emit(this.personalData);
   }
+
+  setUrlInputs() {
+    if (!this.personalData.github && !this.personalData.linkedIn) {
+      this.personalData.github = urlGithub;
+      this.personalData.linkedIn = urlLinkedin;
+    }
+  }
+
+  // validateUrlInputs() {
+  //   this.personalData.github = urlGithub.concat(this.personalData.github);
+
+  //   this.onFormsChange();
+  // }
 }
