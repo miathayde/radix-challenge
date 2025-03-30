454c655d9ed823427c312b258d6a35cf919c1457
Michelle Athayde <michelle.estevinho@hotmail.com>
2025-03-30 01:24:45 -0300

    Ajuste para data seguir padrão DD-MM-AAAA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'}
+  ]
 };
